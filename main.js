38407358706e54c9a456593a2b50619b1e99a003
Luna Amadeus <leo.amadeus.scott@gmail.com>
2026-04-28 12:22:22 +1000

    boob

--- main.js
+++ main.js
@@ -24,6 +24,9 @@ const getHumanChoice = () => {
         case "s" :
             return "scissors";
             break;
+         case "boob" :
+            return "boob";
+            break;
         default :
             console.log("Please make a valid choice.");
             return getHumanChoice();
@@ -35,6 +38,10 @@ const playRound = () => {
     let computerChoice = getComputerChoice();
     let humanChoice = getHumanChoice();
     switch (true) {
+         case (humanChoice === "boob") :
+            humanScore = 69;
+            return 'nice'
+            break;
         case (humanChoice === computerChoice) :
             computerScore += 0.5;
             humanScore += 0.5;
@@ -54,6 +61,9 @@ const playRound = () => {
 const playGame = () => {
     for (let round = 1; round <= 5; round ++) {
         console.log(playRound());
+        if (humanScore === 69) {
+            break;
+        }
     }
     if (computerScore > humanScore) {
         return `The computer won the game. Better luck next time! You lost by ${computerScore - humanScore}.`;
